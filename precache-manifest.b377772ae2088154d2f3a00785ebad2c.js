42bb5c938adee36227733981e8f3d5f00dd72893
xiezhichun <xiezhichun@youzan.com>
2018-11-06 15:39:35 +0800

    Updates

---
+++ precache-manifest.b377772ae2088154d2f3a00785ebad2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eafd1713c3b8c223f946",
+    "url": "/ZhichX/react-study-tutorial.git/static/js/runtime~main.eafd1713.js"
+  },
+  {
+    "revision": "7372a346895bd1d87d6c",
+    "url": "/ZhichX/react-study-tutorial.git/static/js/main.7372a346.chunk.js"
+  },
+  {
+    "revision": "5b966dc2e69f15274453",
+    "url": "/ZhichX/react-study-tutorial.git/static/js/1.5b966dc2.chunk.js"
+  },
+  {
+    "revision": "7372a346895bd1d87d6c",
+    "url": "/ZhichX/react-study-tutorial.git/static/css/main.bc60c592.chunk.css"
+  },
+  {
+    "revision": "e282b60c3f38fb561a2d754816e72b4e",
+    "url": "/ZhichX/react-study-tutorial.git/index.html"
+  }
+];
